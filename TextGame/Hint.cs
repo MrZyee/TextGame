1762f63802e69b1df3d31a444e2ebf447f416f61
bartlomiej.koceniak <bartlomiej.koceniak@E440>
2023-03-08 10:26:09 +0100

    Dodaj pliki projektów.

---
+++ TextGame/Hint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGame
+{
+    internal class Hint
+    {
+       
+        public static int HintEvenNumbers(int number)
+        {
+            int tries = 0;
+            if (number % 2 ==0 )
+            {
+                Console.WriteLine("This is your 4th attempt. Here is the first hint: numer is even");
+
+                if (tries > 3 && number >= 50 && number % 2 == 0)
+                {
+                    Console.WriteLine("The number is in the range of 50-100 ");
+                }
+                else if (tries > 3 && number <= 50 && number % 2 == 0)
+                {
+                    Console.WriteLine("The number is in the range of 1-50 ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("This is your 4th attempt. Here is the first hint: numer is not even");
+
+                if (tries > 3 && number >= 50 && number % 2 == 1)
+                {
+                    Console.WriteLine("The number is in the range of 50-100 ");
+                }
+                else if (tries > 3 && number <= 50 && number % 2 == 1)
+                {
+                    Console.WriteLine("The number is in the range of 1-50 ");
+                }
+            }
+            return tries;
+        }
+    }
+}
---
+++ TextGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TextGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, guess, tries = 0;
+            Random random = new Random();
+            Hint hint = new Hint();
+
+            number = random.Next(1, 101); // The function draws any number between 1 and 100
+
+            Console.WriteLine("Welcome! Guess the number from 1 to 100");
+
+            do
+            {
+                Console.WriteLine("Enter the number: ");
+                guess = int.Parse(Console.ReadLine());
+                tries++;
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Too few! Try again");
+                    if (tries > 3)
+                    {
+                        Hint.HintEvenNumbers(number);
+                    }
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Too much! Try again");
+                    if (tries > 3)
+                    {
+                        Hint.HintEvenNumbers(number);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed it for " + tries + " togethe." );
+                }
+
+            } while (guess != number);
+
+            Console.WriteLine("Thank you for the game!");
+            Console.ReadKey();
+        }
+    }
+}
